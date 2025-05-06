9601c65dae1fe3bfdf7026265c3ec986085dc6f3
GABRIEL DA SILVA SOARES <24122283@dsn127m1246734.cfp127aluno.local>
2025-05-06 17:25:04 -0300

    termino

--- app/src/main/java/br/senai/sp/jandira/profile/screens/HomeScreen.kt
+++ app/src/main/java/br/senai/sp/jandira/profile/screens/HomeScreen.kt
@@ -16,18 +16,22 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowForward
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.ArrowForward
 import androidx.compose.material.icons.filled.Email
 import androidx.compose.material.icons.filled.LocationOn
+import androidx.compose.material.icons.filled.Person
 import androidx.compose.material.icons.filled.Phone
 import androidx.compose.material.icons.filled.Star
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
@@ -36,6 +40,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
@@ -233,26 +238,120 @@ fun HomeScreen() {
                 }
             }
             Column(
-                modifier = Modifier
-                    .fillMaxWidth()
-            ){
-                Row(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(20.dp)
-                ){
-                    Icon(
-                        Icons.Default.Email,
-                        contentDescription = "Email",
-                        tint = Color(0xff9489f8)
-                    )
-                    Text(
-                        text = stringResource(R.string.email),
-                        color = Color.Black,
-                        fontWeight = FontWeight.Bold,
+                modifier = Modifier.padding(20.dp)
+            ) {
+                Column {
+                    Row(verticalAlignment = Alignment.CenterVertically) {
+                        Icon(Icons.Filled.Email, contentDescription = "Email", tint = Color(0xff9489f8))
+                        Text(text = stringResource(id = R.string.email), fontSize = 16.sp, fontWeight = FontWeight.W500, modifier =
+                        Modifier.padding(start = 5.dp))
+                    }
+                    Column(modifier = Modifier
+                        .padding(start = 30.dp)
+                        .padding(top = 10.dp)) {
+                        Text(text = stringResource(R.string.oficial), color = Color.Gray, fontSize = 13.sp)
+                        Text(text = "gabriel@outlook.com", fontSize = 15.sp, fontWeight = FontWeight.W400)
+                        Text(text = stringResource(R.string.personal), color = Color.Gray, fontSize = 13.sp, modifier = Modifier.padding(top = 10.dp))
+                        Text(text = "gabriel@gmail.com", fontSize = 15.sp, fontWeight = FontWeight.W400)
+                    }
+                }
+
+                HorizontalDivider(Modifier.padding(vertical = 10.dp))
+
+                Column {
+                    Row(verticalAlignment = Alignment.CenterVertically) {
+                        Icon(Icons.Filled.Phone, contentDescription = "Email", tint = Color(0xff9489f8))
+                        Text(text = stringResource(id = R.string.phone), fontSize = 16.sp, fontWeight = FontWeight.W500, modifier =
+                        Modifier.padding(start = 5.dp))
+                    }
+                    Column(modifier = Modifier
+                        .padding(start = 30.dp)
+                        .padding(top = 10.dp)) {
+                        Text(text = stringResource(R.string.mobile), color = Color.Gray, fontSize = 13.sp)
+                    }
+                }
+
+                HorizontalDivider(Modifier.padding(vertical = 10.dp))
+
+
+                Row (horizontalArrangement = Arrangement.SpaceBetween, verticalAlignment = Alignment.CenterVertically, modifier = Modifier.fillMaxWidth()){
+                    Column {
+                        Row(verticalAlignment = Alignment.CenterVertically) {
+                            Icon(painter = painterResource(id = R.drawable.whatsappa), contentDescription = "Email", tint = Color(0xff9489f8))
+                            Text(text = stringResource(id = R.string.team), fontSize = 16.sp, fontWeight = FontWeight.W500, modifier =
+                            Modifier.padding(start = 5.dp))
+                        }
+                        Column(modifier = Modifier
+                            .padding(start = 30.dp)
+                            .padding(top = 10.dp)) {
+                            Text(text = "Project operation team", fontSize = 15.sp, fontWeight = FontWeight.W400)
+                        }
+                    }
+
+                    Box(modifier = Modifier
+                        .clip(shape = CircleShape)
+                        .size(40.dp)
+                        .background(color = Color(0xff2f2289))){
+                        Icon(Icons.AutoMirrored.Filled.ArrowForward, contentDescription = "", tint = Color.White, modifier = Modifier.align(alignment = Alignment.Center))
+                    }
+                }
+
+                HorizontalDivider(Modifier.padding(vertical = 10.dp))
+
+                Row (horizontalArrangement = Arrangement.SpaceBetween, verticalAlignment = Alignment.CenterVertically, modifier = Modifier.fillMaxWidth()){
+                    Column {
+                        Row(verticalAlignment = Alignment.CenterVertically) {
+                            Icon(Icons.Filled.Person, contentDescription = "Email", tint = Color(0xff9489f8))
+                            Text(text = stringResource(id = R.string.leads), fontSize = 16.sp, fontWeight = FontWeight.W500, modifier =
+                            Modifier.padding(start = 5.dp))
+                        }
+                        Column(modifier = Modifier
+                            .padding(start = 30.dp)
+                            .padding(top = 10.dp)) {
+                            Text(text = "Gabriel", fontSize = 15.sp, fontWeight = FontWeight.W400)
+                        }
+                    }
+
+                    Box(modifier = Modifier
+                        .clip(shape = CircleShape)
+                        .size(40.dp)
+                        .background(color = Color(0xff2f2289))){
+                        Icon(Icons.AutoMirrored.Filled.ArrowForward, contentDescription = "", tint = Color.White, modifier = Modifier.align(alignment = Alignment.Center))
+                    }
+                }
+
+                Row(modifier = Modifier.fillMaxWidth().padding(top = 20.dp), horizontalArrangement = Arrangement.SpaceAround) {
+                    Button(
+                        onClick = {}, colors = ButtonDefaults.buttonColors(
+                            containerColor = Color.Transparent
+                        ),
                         modifier = Modifier
-                            .size(30.dp)
-                    )
+                            .border(1.dp, shape = RoundedCornerShape(30.dp), color = Color.Gray)
+                            .width(200.dp))
+                    {
+
+                        Row {
+                            Icon(painter = painterResource(R.drawable.user), contentDescription = "", tint = Color(0xff2f2289))
+                            Text(text = stringResource(id =R.string.contact), color = Color(0xff2f2289), modifier = Modifier.padding(start = 10.dp), fontSize = 15.sp)
+                        }
+
+                    }
+
+                    Button(
+                        onClick = {}, colors = ButtonDefaults.buttonColors(
+                            containerColor = Color.Transparent
+                        ),
+                        modifier = Modifier
+                            .border(1.dp, shape = RoundedCornerShape(30.dp), color = Color.Gray)
+                            .width(70.dp))
+                    {
+                        Icon(
+                            painter = painterResource(R.drawable.user),
+                            contentDescription = "",
+                            tint = Color(0xff2f2289)
+                        )
+
+                    }
                 }
             }
         }
